3051af63558c94ea0d60e149523b0a279875b677
Emiliano Musso <58050537+EmilianoMusso@users.noreply.github.com>
2021-06-12 19:49:24 +0200

    Add project files.

---
+++ EmilianoMusso.EFCoreExtensions.DbToInMemory/DatabaseToInMemory.cs
@@ -0,0 +1,34 @@
+﻿using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmilianoMusso.EFCoreExtensions.DbToInMemory
+{
+    public class DatabaseToInMemory
+    {
+        private readonly DbContext _context;
+        private readonly string _connectionString;
+        private readonly bool _randomOrder;
+        public DatabaseToInMemory(DbContext context, string connectionString, bool randomOrder = true)
+        {
+            _context = context;
+            _connectionString = connectionString;
+            _randomOrder = true;
+        }
+
+        public DatabaseToInMemory LoadTable<T>(int topRecords = 10) where T : class, new()
+        {
+            _context.LoadTableExt<T>(_connectionString, topRecords, _randomOrder);
+            return this;
+        }
+
+        public int PersistToMemory()
+        {
+            return _context.SaveChanges();
+        }
+
+        public async Task<int> PersistToMemoryAsync()
+        {
+            return await _context.SaveChangesAsync();
+        }
+    }
+}
